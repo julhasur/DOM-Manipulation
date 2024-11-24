03001ef2da2c32a7de9d0992975577c41b1be85f
Julhasur Chowdhury <37052892+julhasur@users.noreply.github.com>
2024-11-24 00:05:59 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+let mainEL = document.querySelector("main");
+mainEL.style.backgroundColor = "var(--main-bg)";
+mainEL.innerHTML = "<h1>Dom Manipulation</h1>";
+mainEL.classList.add("flex-ctr");
