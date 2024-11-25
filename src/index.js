73b3794d74c7822df7b8c402c4ac5fbc4dcc756e
Julhasur Chowdhury <37052892+julhasur@users.noreply.github.com>
2024-11-25 03:17:57 +0000

    fixed the nav items part 3 issue of nav elements not showing up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import "./styles.css";
+// Menu data structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+//working on part one
 let mainEL = document.querySelector("main");
 mainEL.style.backgroundColor = "var(--main-bg)";
 mainEL.innerHTML = "<h1>Dom Manipulation</h1>";
 mainEL.classList.add("flex-ctr");
+
+//part2 creating a menuebar
+let topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+//part3
+menuLinks.forEach(function (link) {
+  const ancor = document.createElement("a");
+  ancor.setAttribute("href", link.href);
+  ancor.textContent = link.text;
+  topMenuEl.append(ancor);
+});
